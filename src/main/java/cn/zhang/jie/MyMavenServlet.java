c80249820ba682d4953b6b5f963b613a50ed6b20
zhangjie <zhangjieatms@outlook.com>
2020-03-27 18:13:24 +0800

    initial commit

---
+++ src/main/java/cn/zhang/jie/MyMavenServlet.java
@@ -0,0 +1,20 @@
+package cn.zhang.jie;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyMavenServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("maven ...  doget");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
