cec6c151b1890a1210279410a320ed24f9304186
zhangjie <zhangjieatms@outlook.com>
2020-03-27 18:19:41 +0800

    1

--- src/main/java/cn/zhang/jie/MyMavenServlet.java
+++ src/main/java/cn/zhang/jie/MyMavenServlet.java
@@ -10,11 +10,11 @@ public class MyMavenServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("maven ...  doget11");
+        System.out.println("maven ...  doget1221");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println(11);
     }
 }
